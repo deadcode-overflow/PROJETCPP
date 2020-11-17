87d4647a009a05a681ab59171fc9f8e7a519bdbf
Dylan Haral <dylan.haral@ens.uvsq.fr>
2020-11-17 23:50:54 +0100

    réorganisation du fichier et mise à jour

--- ArbreB.h
+++ ArbreB.h
@@ -21,20 +21,27 @@ class ArbreB : public Sommet
 		//ArbreB(ArbreB& A); //constructeur par copie (non utilisé pour l'instant)
 		~ArbreB();
 
-		ArbreB& ajouter(Sommet& s); //operateur +  (A + s) - fait
-		ArbreB& supprimer(Sommet& s); //operateur - (A - s) - a faire
-		Sommet& rechercher(Sommet& s); //fait
-		ArbreB& modifier(Sommet& s, char c, int f); //fait
-		ArbreB& fusionner(ArbreB& A); //operateur + (A + A) - a faire
-		ArbreB& decomposer(); // A -> Ad + Ag - a faire
-		void afficher(ArbreB* A); //operateur << - fait
-
 		Sommet& getSommet();
 		ArbreB* getDroite();
 		ArbreB* getGauche();
 		ArbreB* getPrecedent();
 
-		//friend ostream& operator<<(ostream& flux, ArbreB& A); // cout << A
-		//void supprimerArbre(ArbreB* A); //inutile pour l'instant
+		ArbreB& ajouter(Sommet& s); //fait
+		ArbreB& supprimer(Sommet& s); //fait
+		Sommet& rechercher(Sommet& s); //fait
+		ArbreB& modifier(Sommet& s, char c, int f); //fait
+		ArbreB& fusionner(ArbreB& A); //a faire
+		ArbreB& decomposer(); //a faire
+		void afficher(ArbreB* A); //fait - (sert pour << ?)
+
+		bool operator ==(ArbreB* A); //sert pour constructeur par copie - (erreur à corriger : retourne toujours faux)
+		friend ostream& operator<<(ostream& flux, ArbreB& A); //a faire
+
+		//--------------BONUS--------------
+		int hauteur(ArbreB* A, int hg, int hd); //calcule la hauteur de l'arbre
+		int nombre_element(ArbreB* A); //calcule le nombre d'élément dans l'arbre
+		ArbreB* min(ArbreB* A); //retourne l'arbre (feuille) minimal de l'arbre - sert pour supprimer
+		ArbreB* max(ArbreB* A); //retourne l'arbre (feuille) maximal d'arbre - sert pour supprimer
+		bool egalite(ArbreB* current, ArbreB* A); //vérifie de manière récursive si deux arbres sont égaux - sert pour l'operateur ==
 };
 #endif
