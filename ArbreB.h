b50c89571f7c9f0cd36744a640f5ce254dc7a6b4
Dylan Haral <dylan.haral@ens.uvsq.fr>
2020-11-19 18:03:51 +0100

    modification fusionner et afficher

--- ArbreB.h
+++ ArbreB.h
@@ -30,9 +30,11 @@ class ArbreB : public Sommet
 		ArbreB& supprimer(Sommet& s); //fait
 		Sommet& rechercher(Sommet& s); //fait
 		ArbreB& modifier(Sommet& s, char c, int f); //fait
-		ArbreB& fusionner(ArbreB& A); //a faire
+		ArbreB& fusionner(ArbreB* A); //a faire
 		ArbreB& decomposer(); //a faire
 		void afficher(ArbreB* A); //fait - (sert pour << ?)
+
+		void print_t(int hauteur, int cote, ArbreB* A); //affichage ameliore
 
 		bool operator ==(ArbreB* A); //sert pour constructeur par copie - (erreur à corriger : retourne toujours faux)
 		friend ostream& operator<<(ostream& flux, ArbreB& A); //a faire
@@ -41,7 +43,7 @@ class ArbreB : public Sommet
 		int hauteur(ArbreB* A, int hg, int hd); //calcule la hauteur de l'arbre
 		int nombre_element(ArbreB* A); //calcule le nombre d'élément dans l'arbre
 		ArbreB* min(ArbreB* A); //retourne l'arbre (feuille) minimal de l'arbre - sert pour supprimer
-		ArbreB* max(ArbreB* A); //retourne l'arbre (feuille) maximal d'arbre - sert pour supprimer
+		ArbreB* max(ArbreB* A); //retourne l'arbre (feuille) maximal de l'arbre - sert pour supprimer
 		bool egalite(ArbreB* current, ArbreB* A); //vérifie de manière récursive si deux arbres sont égaux - sert pour l'operateur ==
 };
 #endif
