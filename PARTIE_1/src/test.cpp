8d01af03a20c6fe6f25737b05cf4b8f7f55d6780
Dylan Haral <dylan.haral@ens.uvsq.fr>
2021-01-13 23:36:35 +0100

    mise a jour partie 1

--- PARTIE_1/src/test.cpp
+++ PARTIE_1/src/test.cpp
@@ -10,57 +10,69 @@ int test() {
 
     //--------------test-des-sommets------------------
     cout << endl;
-    
     //test affichage constructeur par valeur
 	Sommet s('A', 4);
+    cout << "constructeur par valeur" << endl;
     cout <<  "s -> " << s << endl;
+    cout << "----------------------------------" << endl;
     
     //test affichage constructeur par défaut
     Sommet defaut;
+    cout << "constructeur par defaut" << endl;
     cout << "def -> " << defaut << endl;
+    cout << "----------------------------------" << endl;
     
     //test affectation
     Sommet s2('B', 1);
+    cout << "affectation de def = s2" << endl;
+    cout << "def -> " << defaut << endl;
     cout << "s2 -> " << s2 << endl;
     defaut = s2;
-    cout << "affectation def = s2" << endl;
     cout << "def -> " << defaut << endl;
+    cout << "----------------------------------" << endl;
     
     //test égalité
     Sommet s3('A', 4);
+    cout << "comparaison de s == s3" << endl;
+    cout << "s -> " << s << endl;
     cout << "s3 -> " << s3 << endl;
-    cout << "comparaison s == s3" << endl;
     if(s == s3)
         cout << "s et s3 sont égaux" << endl;
     else
         cout << "s et s3 ne sont pas égaux" << endl;
+    cout << "----------------------------------" << endl;
     cout << endl;
     
     //test affichage constructeur par copie
     Sommet copie(s);
-    cout << "copie de s" << endl;
-    cout << "sommmet 'copie' de 's' -> " << copie << endl;
+    cout << "constructeur par copie de s" << endl;
+    cout << "sommmet copie -> " << copie << endl;
+    cout << "----------------------------------" << endl;
     //-------------------------------------------------
 
     //--------------test-de-l'arbre-binaire------------
-    
+    cout << endl;
     //test affichage constructeur par défaut
     ArbreB AbrDefaut;
-    cout << "creation de d'un arbre par défaut" << endl;
+    cout << "constructeur par défaut" << endl;
     AbrDefaut.afficher(&AbrDefaut);
-    
+    cout << "----------------------------------" << endl;
+
     //test affichage constructeur par valeur
     ArbreB A(s);
-    cout << "creation de l'arbre de racine s" << endl;
+    cout << "constructeur avec racine de s" << endl;
     A.afficher(&A);
-    
+
     //test ajout d'un sommet dans l'arbre
     A.ajouter(s2);
-    cout << "ajout de s2 dans l'arbre" << endl;
+    cout << "ajout de s2 dans l'arbre A" << endl;
     A.afficher(&A);
     A.print_t(0,0,&A);
-    
+    cout << "----------------------------------" << endl;
+    cout << endl;
+
     //test ajout de plusieurs sommet dans l'arbre
+    cout << "ajout de plusieurs sommets dans l'arbre A" << endl;
     Sommet s4('C',1);
     cout << "s4 -> " << s4 << endl;
     Sommet s5('D',2);
@@ -73,37 +85,46 @@ int test() {
     A.ajouter(s5);
     A.ajouter(s6);
     A.ajouter(s7);
-    cout << "ajout de plusieurs sommets dans l'arbre" << endl;
     A.afficher(&A);
     A.print_t(0,0,&A);
-    
+    cout << "----------------------------------" << endl;
+    cout << endl;
+
     //test rechecher un sommet dans l'arbre
     Sommet sommetArbre = A.rechercher(s5);
-    cout << "recherche du sommet s5 dans l'arbre" << endl;
+    cout << "recherche du sommet s5 dans l'arbre A" << endl;
+    cout << "s5 -> " << s5 << endl;
     cout << "sommet de l'arbre -> " << sommetArbre << endl;
-    
+    cout << "----------------------------------" << endl;
+
     //test modifier un sommmet dans l'arbre
     A.modifier(s6, 'K', 6);
-    cout << "modification du sommet s6 dans l'arbre par 'K' '6'" << endl;
+    cout << "modification du sommet s6 dans l'arbre A par 'K' '6'" << endl;
     cout << "s6 -> " << s6 << endl;
     A.afficher(&A);
     A.print_t(0,0,&A);
-    
+    cout << "----------------------------------" << endl;
+    cout << endl;
+
     //test hauteur de l'arbre
     cout << "hauteur de l'arbre A : " << A.hauteur(&A,0,0) << endl;;
-    
+
     //test nombre d'élément dans l'arbre
     cout << "nombre d'element dans l'arbre A : " << A.nombre_element(&A) << endl;
+    cout << "----------------------------------" << endl;
     cout << endl;
     
     //test de suppression d'un sommet dans l'arbre
     A.supprimer(s5);
-    cout << "s5 -> " << s5;
     cout << "suppression du sommet s5 dans l'arbre A" << endl;
+    cout << "s5 -> " << s5;
     A.afficher(&A);
     A.print_t(0,0,&A);
-    
+    cout << "----------------------------------" << endl;
+    cout << endl;
+
     //test affichage arbre amélioré
+    cout << "affichage de l'arbre N en arborescence" << endl;
     Sommet s8('G', 1);
     cout <<  "s8 -> " << s8 << endl;
     Sommet s9('H', 2);
@@ -159,14 +180,20 @@ int test() {
     N.ajouter(s8);
     N.ajouter(s17);
     N.print_t(0,0,&N);
-    
+    cout << "----------------------------------" << endl;
+    cout << endl;
+
     //test constructeur arbre copie
     ArbreB N_copie(&N);
-    cout << "arbre N_copie " << endl;
+    cout << "constructeur par copie de l'arbre N" << endl;
+    cout << "N_copie";
     N_copie.print_t(0,0,&N_copie);
     N.print_t(0,0,&N);
+    cout << "----------------------------------" << endl;
+    cout << endl;
 
     //test fusionner deux arbres binaires
+    cout << "fusion entre N et A" << endl;
     A.ajouter(s23);
 	A.ajouter(s11);
     A.ajouter(s18);
@@ -177,29 +204,36 @@ int test() {
     A.ajouter(s27);
     A.modifier(s12, 'E', 3);
 
-    cout << "Arbre A" << endl;
+    cout << "Arbre A avant fusion";
     cout << endl;
     A.print_t(0,0,&A);
 
-    cout << "Arbre N avant fusion" << endl;
+    cout << "Arbre N avant fusion";
     cout << endl;
     N.print_t(0,0,&N);
 
     N.fusionner(&A);
 
-    cout << "Arbre N après fusion" << endl;
+    cout << "Arbre N après fusion";
+    cout << endl;
+    A.print_t(0,0,&A);
+
+    cout << "Arbre N après fusion";
     cout << endl;
     N.print_t(0,0,&N);
+    cout << "----------------------------------" << endl;
+    cout << endl;
 
     //test decomposition
+    cout << "decomposition de l'arbre N fusion" << endl;
     ArbreB AbrDroit, AbrGauche;
     N.decomposer(&AbrGauche, &AbrDroit, &N);
    
-    cout << "AbrGauche de N fusion" << endl;
+    cout << "arbre gauche de N fusion";
     cout << AbrGauche.getSommet();
     AbrGauche.print_t(0,0,&AbrGauche);
     
-    cout << "AbrDroit de N fusion" << endl;
+    cout << "arbre droit de N fusion";
     cout << AbrDroit.getSommet();
     AbrDroit.print_t(0,0,&AbrDroit);
     //-------------------------------------------------
